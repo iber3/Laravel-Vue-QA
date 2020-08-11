9cb28764c54f240742dd1198a16cde5af279d868
iber3 <56194270+iber3@users.noreply.github.com>
2020-08-11 09:21:20 +0300

    test

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -59,11 +59,11 @@
                                     
                                 </div>
                                 <div class="col-4">
-                                    {{-- @include ('shared._author', [
+                                    @include ('shared._author', [
                                         'model' => $answer,
                                         'label' => 'answered'
-                                        ]) --}}
-                                <user-info :model="{{$answer}}", label="Answered"></user-info>
+                                        ])
+                                {{-- <user-info :model="{{$answer}}", label="Answered"></user-info> --}}
                             </div>
                             </div>
                             
